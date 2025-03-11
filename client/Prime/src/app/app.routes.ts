931e85d1177922577bf90b7d0b6c5a953b159c43
Nathandiv <k.n.hlungwani@gmail.com>
2025-03-11 11:19:42 +0200

    added routing for all the pages

--- client/Prime/src/app/app.routes.ts
+++ client/Prime/src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ShopComponent } from './pages/shop/shop.component';
+import { TourComponent } from './pages/tour/tour.component';
+import { MusicComponent } from './pages/music/music.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path:'',component:HomeComponent
+    },
+
+    {
+        path:'about',component:AboutComponent
+    },
+
+    {
+        path:'music',component:MusicComponent
+    },
+
+    {
+        path:'tour',component:TourComponent
+    },
+
+
+    {
+        path:'shop',component:ShopComponent
+    },
+
+    {
+        path:'contact',component:ContactComponent
+    },
+
+
+];
