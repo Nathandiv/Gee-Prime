4092c0d9356f8f84696208ff74cd3db856143fdc
Nathandiv <k.n.hlungwani@gmail.com>
2025-03-17 13:44:33 +0200

    maded changes and adjustment of the section below

--- client/Prime/src/app/pages/home/home.component.ts
+++ client/Prime/src/app/pages/home/home.component.ts
@@ -1,17 +1,16 @@
 import { Component, Input } from '@angular/core';
-import { NavbarComponent } from "../../Share-UI/navbar/navbar.component";
-import { FooterComponent } from "../../Share-UI/footer/footer.component";
+import { NavbarComponent } from '../../Share-UI/navbar/navbar.component';
+import { FooterComponent } from '../../Share-UI/footer/footer.component';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent,CommonModule],
+  imports: [NavbarComponent, FooterComponent, CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
   @Input() imageUrl: string = '';
   @Input() title: string = '';
   @Input() artist: string = '';
@@ -19,20 +18,23 @@ export class HomeComponent {
   platforms = [
     {
       imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/19/Spotify_logo_without_text.svg',
-      name: 'Spotify'
+      name: 'Spotify',
+      url: 'https://www.spotify.com/'
     },
     {
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/9/9e/Amazon_Music_logo.svg',
-      name: 'Amazon Music'
+      imageUrl: 'https://i.pinimg.com/474x/df/74/c3/df74c38d4de71310a2a72ac5a420cb4a.jpg',
+      name: 'Apple Music',
+      url: 'https://music.apple.com/'
     },
     {
       imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/0/09/YouTube_full-color_icon_%282017%29.svg',
-      name: 'YouTube'
+      name: 'YouTube',
+      url: 'https://www.youtube.com/'
     },
     {
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/3/3a/Pandora_app_logo.svg',
-      name: 'Pandora'
-    }
+      imageUrl: 'https://i.pinimg.com/474x/05/ee/55/05ee55960ac7c0162c2f2ee988dc7783.jpg',
+      name: 'Deezer',
+      url: 'https://www.deezer.com/'
+    },
   ];
-
 }
