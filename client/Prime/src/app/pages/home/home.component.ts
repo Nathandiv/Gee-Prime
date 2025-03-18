919f21588afb69bdf7ea27e1702cbcd218f609df
Nathandiv <k.n.hlungwani@gmail.com>
2025-03-18 11:00:24 +0200

    made the images also to pull from the back end

--- client/Prime/src/app/pages/home/home.component.ts
+++ client/Prime/src/app/pages/home/home.component.ts
@@ -157,4 +157,17 @@ export class HomeComponent {
       },
     ],
   };
+
+  images = [
+    {
+      src: 'https://mp3bullet.ng/wp-content/uploads/2025/02/Gee-Primes-Timeless-Confessions-Album-A-Soulful-Afro-House-Journey.jpg',
+      alt: 'office content 1',
+      height: '220px',
+    },
+    {
+      src: 'https://images.caxton.co.za/wp-content/uploads/sites/112/2024/03/A7403411_79617-e1709890587845.jpg',
+      alt: 'office content 2',
+      height: '250px',
+    },
+  ];
 }
