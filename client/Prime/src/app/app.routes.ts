5ee8af40c71387951a3b9ffc37f4e95c5d66c300
Nathandiv <k.n.hlungwani@gmail.com>
2025-04-11 14:31:31 +0200

    added routing and landing page

--- client/Prime/src/app/app.routes.ts
+++ client/Prime/src/app/app.routes.ts
@@ -5,11 +5,15 @@ import { ContactComponent } from './pages/contact/contact.component';
 import { ShopComponent } from './pages/shop/shop.component';
 import { TourComponent } from './pages/tour/tour.component';
 import { MusicComponent } from './pages/music/music.component';
+import { LandingComponent } from './pages/landing/landing.component';
 
 export const routes: Routes = [
 
     {
         path:'',component:HomeComponent
+    },
+    {
+        path:'landing',component:LandingComponent
     },
 
     {
